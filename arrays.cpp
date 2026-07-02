9cd331f8c0badd0f8d4ea9918ece62130be7e821
kiran umarani <kiranumarni200@gmail.com>
2026-07-03 05:19:25 +0530

    fist commit

---
+++ arrays.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int arr[5];
+    cout <<"enter 5 numbers: ";
+    for(int i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    
+    }
+    cout<<"the numbers are: ";
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<" ";
+}
+        
+    
+}
